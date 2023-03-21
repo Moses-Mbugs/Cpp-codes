6a4ad3dfddbeadd2a554188cc97c1068dca1c2df
Moses-Mbugs <mbuguam323@gmail.com>
2023-03-21 17:47:23 +0300

    odd number generator

---
+++ odd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // simple odd number
+    
+    int total = 100; // number of odd numbers to count to
+    int i = 1; // starting fromm 1
+    int count = 0;
+
+    while(count < total){
+        if(i % 2 , != 0){
+            cout << i << endl;
+            count++
+        }
+        i++;
+    }
+    return 0;
+}
