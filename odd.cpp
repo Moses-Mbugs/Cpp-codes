9b8eed93016e6ab02916b19829d205883c726a92
Moses Mbugua <126574366+Moses-Mbugs@users.noreply.github.com>
2023-03-22 11:37:46 +0300

    Update odd.cpp

--- odd.cpp
+++ odd.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     // simple odd number
     
-    int total = 100; // number of odd numbers to count to
+    int total = 100; // number of odd numbers to count 
     int i = 1; // starting fromm 1
     int count = 0;
 
